fcdb1d15b7e56a00c89f63ff7905eec30e1b4890
Olyvia Hix <olyvia4@gmail.com>
2023-04-13 15:44:39 -0700

    create user and login cards created as well as social post card

--- socialsociety/src/index.js
+++ socialsociety/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
