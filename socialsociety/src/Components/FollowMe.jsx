c8fc6332af3f5ebb0dda404fa00bcf6469a9953d
Olyvia Hix <olyvia4@gmail.com>
2023-04-13 18:25:37 -0700

    updated push to Olyvia branch

---
+++ socialsociety/src/Components/FollowMe.jsx
@@ -0,0 +1,11 @@
+
+function FollowMe() {
+    return(
+        <div className = 'Follow'>
+            Followers: 
+            Following:
+            Post:
+        </div>
+    )
+}
+export default FollowMe;
